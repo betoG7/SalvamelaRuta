89a3c7879bb87db9069ab45993bb88dbc9bcfa46
betoG7 <humbertogarciag7@gmail.com>
2016-08-31 22:46:02 -0500

    Initial commit

---
+++ app/src/main/java/mx/com/hopped/salvamelaruta/Mapa_Ejemplo.java
@@ -0,0 +1,43 @@
+package mx.com.hopped.salvamelaruta;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Mapa_Ejemplo extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng lp = new LatLng(20.3464179,-102.0284554);
+        CameraUpdate camera = CameraUpdateFactory.newLatLngZoom(lp, 15);
+
+        mMap.addMarker(new MarkerOptions().position(lp).title("La Piedad"));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.moveCamera(camera);
+
+    }
+}
